fe581ae8f5e1086fc1cc9ceb7cba846638f18fa2
Stephanie Waterson <stephaniemariewaterson@gmail.com>
2024-07-05 16:32:56 +0100

    web socket endpoints

--- index.js
+++ index.js
@@ -43,6 +43,10 @@ io.on("connection", (socket) => {
     console.log(`User with ID : ${socket.id} joined room ${data}`);
   });
 
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
   socket.on("disconnect", () => {
     console.log("user disconnected", socket.id);
   });
